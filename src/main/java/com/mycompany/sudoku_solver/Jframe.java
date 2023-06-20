51eaea5c3584e9c5301509d31a483729ad2d218d
akshita-gupta-1995 <137076107+akshita-gupta-1995@users.noreply.github.com>
2023-06-20 23:25:18 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/sudoku_solver/Jframe.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sudoku_solver;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ayush
+ */
+public class Jframe extends JFrame {
+
+    public Jframe(String reset) {
+    }
+    
+}
